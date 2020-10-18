05b3bffbf484f04ca448d512c80cb32b27870486
Iris Chen <jin2006613@gmail.com>
2020-10-18 12:31:19 +0800

    revised 0 times message

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 pwd = 'a123456'
 i = 3 #剩餘次數
 while i > 0:
+	i = i - 1
 	ans = input('請輸入密碼：')
 	if pwd == ans:
 		print('登陸成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤！您還有', i, '次機會')
+		print('密碼錯誤！')
+		if i > 0:
+			print('您還有', i, '次機會')
+		else:
+			print('沒機會嘗試了')
 
 
 
