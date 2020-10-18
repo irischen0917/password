6ffdc6fb3608538f3ee44fea5da2c5cc7b821d5d
Iris Chen <jin2006613@gmail.com>
2020-10-18 12:19:02 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,19 @@
+pwd = 'a123456'
+i = 3 #剩餘次數
+while True:
+	ans = input('請輸入密碼：')
+	if pwd == ans:
+		print('登陸成功')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤！您還有', i, '次機會')
+		if i == 0:
+			break
+
+
+
+
+	
+	
+	
